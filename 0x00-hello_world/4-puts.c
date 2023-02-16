98d986d39459e840ae3b585b7e44b7274e1f7ea7
Antony Nzau <nzauanthony7@gmail.com>
2023-02-16 05:36:17 -0800

    hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+/**
+ * main - Entry point
+ * Return : Always 0 (success)
+ */
+int main (void)
+{
+	puts ("programming is like buildingg a multiligual puzzle\n")
+		return 0
+}
