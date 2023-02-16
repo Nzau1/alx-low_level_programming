99a32377359090f6d137d0e39d4fc6fa0dbd4d31
Antony Nzau <nzauanthony7@gmail.com>
2023-02-16 07:27:05 -0800

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints with proper grammar
+ * Return:0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
