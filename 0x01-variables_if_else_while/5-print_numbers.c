f46b3ed402c5877a7657f76821fe75064ba6ea80
Antony Nzau <nzauanthony7@gmail.com>
2023-02-17 04:06:06 -0800

    string of numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10 from 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
