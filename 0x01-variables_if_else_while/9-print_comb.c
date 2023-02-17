0a2c4529b3bca64fcfc067296d68299b2080311f
Antony Nzau <nzauanthony7@gmail.com>
2023-02-17 04:42:44 -0800

    patience,persistence andperspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single digit number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; n++)
+
+	{
+		putchar(m);
+	}
+		if (m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
