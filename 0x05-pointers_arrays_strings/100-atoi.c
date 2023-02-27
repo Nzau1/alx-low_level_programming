de9ac46ef691673c565627c8670d2e7ea3d5ebf9
Antony Nzau <nzauanthony7@gmail.com>
2023-02-27 04:09:28 -0800

    great leaders are willing to sacrifice the numbers to save the people

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int signo = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	{
+		if (s[i] == '-')
+			signo *= -1;
+		i++;
+	}
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+	{
+		if (n >= 0)
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n =n  * 10 - (s[i] -'0');
+			i++;
+		}
+	}
+	signo *= -1;
+	return (n * signo);
+}
