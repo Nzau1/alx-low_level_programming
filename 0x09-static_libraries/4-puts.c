f4f1b43f0e970f1e05ee5cfd489908774386bf8b
Antony Nzau <nzauanthony7@gmail.com>
2023-03-12 09:22:18 -0700

    its been real pleasure doing static libraries this long

---
+++ 0x09-static_libraries/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints with a new line
+ * Return:0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+these are static files done with a lot of recursiveness repeating everything and still getting repeats
