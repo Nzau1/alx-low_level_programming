839c07ed7c33cd97a50fa9464e09303d7298ae0a
Nzau1 <nzauanthony7@gmail.com>
2023-04-10 04:40:19 -0700

    returning the value of bit at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,7 +4,7 @@
 unsigned int binary_to_uint(const char *x);
 void print_binary(unsigned long int b);
 int _putchar(char b);
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int t, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
